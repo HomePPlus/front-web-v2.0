65b9c24e2ec259df098ca370b8513caff18a9ea8
jiva-z <jiva_z@naver.com>
2025-01-26 21:21:47 +0900

    feat: Add MainVideo component

---
+++ src/pages/MainVideo/MainVideo.jsx
@@ -0,0 +1,13 @@
+// src/pages/MainVideo/MainVideo.jsx
+import React from 'react';
+import VideoButton from '../../components/common/VideoButton';
+
+const MainVideo = () => {
+  return (
+    <div className="main-container">
+      <VideoButton />
+    </div>
+  );
+};
+
+export default MainVideo; // default export 추가
